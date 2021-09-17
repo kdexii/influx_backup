f573973724532fddc57764963ff77df3a4f2ded9
kdexii <lasvacations@yandex.ru>
2021-09-17 12:12:53 +0300

    add str and float generator python and push to influx

---
+++ main.py
@@ -0,0 +1,23 @@
+import os
+from influxdb import InfluxDBClient
+from random import choice
+from string import ascii_uppercase
+import random
+
+def connection():
+
+    conn = InfluxDBClient(host='localhost',port=8086)
+    return conn
+def writeData(conn):
+    count = -1
+
+    while count <= 50:
+        count = count + 1
+        string = ''.join(choice(ascii_uppercase) for i in range(12))
+        value = random.randint(1,27)
+        data = f'testData,name={string} value={value}'
+
+        conn.write_points(data,database='test3', protocol='line')
+    # print(conn)
+conn = connection()
+writeData(conn)

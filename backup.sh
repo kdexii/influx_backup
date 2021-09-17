932ceb5228073d8334b4cbc74f9847bdd7312beb
kdexii <lasvacations@yandex.ru>
2021-09-17 15:24:41 +0300

    add desc and comment

--- backup.sh
+++ backup.sh
@@ -1,35 +1,42 @@
 #!/bin/bash
 
 
+#   massive with database list
+arr=("test1" "test2" "test3")
 
+#   inactive function, but may come in handy
 function checkFolder {
-    if ! [ -d $directory ]; then
-        echo 'No directory, create...'
-        mkdir $directory
+    if ! [ -d $directory ]; then #  check directory
+        echo 'Directory not found, create...'
+        mkdir $directory #  if not = create
     else
-        echo 'folder is ready'
+        echo 'folder is ready' #    or the folder exists
     fi
 }
+
 function makeBackUp {
-    arr=("test1" "test2" "test3")
-    
+
     x=0
-    while [ $x -lt 3 ]
+    
+    while [ $x -lt 3 ]  #   default while count
     do
         directory="/home/kdexii/tmp/${arr[$x]}"
-        if ! [ -d $directory ]; then
-        echo 'No directory, create...'
-        mkdir $directory
+        if ! [ -d $directory ]; then    #   check directory
+            echo 'Directory not found, create...'
+            mkdir $directory # if not = create
+            sleep 2s
         else
-            echo 'folder is ready'
+            echo 'Directory found' # or the folder exists
+            sleep 2s
         fi
         # echo "count: $x"
-        echo ${arr[$x]}
-        influxd backup -portable -database ${arr[$x]} $directory
-        x=$(( $x + 1 ))
-
+            # echo ${arr[$x]}
+            echo 'Backup database: '${arr[$x]}
+            influxd backup -portable -database ${arr[$x]} $directory
+            x=$(( $x + 1 ))
+            sleep 1s
     done
 }
 # checkFolder
 makeBackUp
-echo 'run time script: '$SECONDS
+echo 'run time script(sec): '$SECONDS
